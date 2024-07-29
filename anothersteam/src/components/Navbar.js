df7eba06b5abcf50f1ce65992de8a54f99c81cb8
evelyn <etersh810@gmail.com>
2024-07-28 23:35:06 -0400

    Add files for top 3 most played games

--- anothersteam/src/components/Navbar.js
+++ anothersteam/src/components/Navbar.js
@@ -1,24 +1,23 @@
-// src/components/Navar.js
+// src/components/Navbar.js
 
 import React from "react";
 import Link from "next/link";
-import { HomeButton, NavButton } from "./Button"
+import { HomeButton, NavButton } from "./Button";
 
 export default function Navbar() {
-  return (
-    <div className="navbar">
-      <Link href="/" passHref className="">
-        <HomeButton />
-      </Link>
+	return (
+		<div className="navbar">
+			<Link href="/" passHref className="">
+				<HomeButton />
+			</Link>
 
-      <Link href="/" passHref className="">
-        <NavButton name="Most Played Game" />
-      </Link>
+			<Link href="/games/MostPlayedGames" passHref className="">
+				<NavButton name="Most Played Games" />
+			</Link>
 
-      <Link href="/" passHref className="">
-        <NavButton name="Trending" />
-      </Link>
-      
-    </div>
-  );
+			<Link href="/" passHref className="">
+				<NavButton name="Trending" />
+			</Link>
+		</div>
+	);
 }
--- anothersteam/src/pages/index.js
+++ anothersteam/src/pages/index.js
@@ -2,16 +2,14 @@ import React from "react";
 import GameSearch from "@/components/GameSearch";
 
 export default function Home() {
-  return (
-    <>
-      <img src="#" alt="HomeBanner"></img>
-      <h1>HOME</h1>
-      <p>Phase1: Fetching data from Steam Web API</p>
-      <div>Top 3 Most played games</div>
+	return (
+		<>
+			<img src="#" alt="HomeBanner"></img>
+			<h1>HOME</h1>
+			<p>Phase1: Fetching data from Steam Web API</p>
 
-
-      <div>Single Game Data: e.g. 1091500</div>
-      <GameSearch />
-    </>
-  );
+			<div>Single Game Data: e.g. 1091500</div>
+			<GameSearch />
+		</>
+	);
 }
