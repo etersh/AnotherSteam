ea9873596e5b9762e2164b06df493be9c94668b9
n106ky (N.C) <n106ky@gmail.com>
2024-07-28 22:03:21 -0400

    Minor update

--- anothersteam/src/pages/index.js
+++ anothersteam/src/pages/index.js
@@ -9,13 +9,6 @@ export default function Home() {
       <p>Phase1: Fetching data from Steam Web API</p>
       <div>Top 3 Most played games</div>
 
-      1. game name
-      2. Picture
-      3. Original price
-      4. Discounted price
-      5. Discount %
-      6. Until when
-
 
       <div>Single Game Data: e.g. 1091500</div>
       <GameSearch />
