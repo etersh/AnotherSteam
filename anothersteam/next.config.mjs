4666a60b33e937c0a2d109b4797d2956fce10d88
n106ky (N.C) <n106ky@gmail.com>
2024-07-28 23:52:53 -0400

    Debug: Most played games

--- anothersteam/next.config.mjs
+++ anothersteam/next.config.mjs
@@ -1,6 +1,38 @@
-// /** @type {import('next').NextConfig} */
+// // /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://store.steampowered.com/api/:path*', // Proxy to external API
+      },
+    ];
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        module: false,
+        child_process: false,
+        dgram: false,
+        dns: false,
+        http2: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
+
