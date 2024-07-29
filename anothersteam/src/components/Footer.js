04479b0dae852ba3d65d656d3aaebc1cfed5f596
n106ky (N.C) <n106ky@gmail.com>
2024-07-28 20:12:29 -0400

    Defining page layout

---
+++ anothersteam/src/components/Footer.js
@@ -0,0 +1,13 @@
+// src/app/component/Footer.js 
+import React from 'react';
+import '../styles/Footer.module.css'; 
+
+function Footer() {
+  return (
+    <footer className="footer">
+
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ anothersteam/src/components/Header.js
@@ -0,0 +1,13 @@
+// src/app/component/Footer.js 
+import React from 'react';
+// import '../styles/Footer.module.css'; 
+
+function Header() {
+  return (
+    <footer className="header">
+
+    </footer>
+  )
+}
+
+export default Header;
