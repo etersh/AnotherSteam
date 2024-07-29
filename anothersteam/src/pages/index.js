786d61af6c8517db361283d5d6a15a664ce78598
n106ky (N.C) <n106ky@gmail.com>
2024-07-28 20:49:34 -0400

    Update: CSS

--- anothersteam/src/pages/index.js
+++ anothersteam/src/pages/index.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
     <>
-      Home
+      <img src="#" alt="HomeBanner"></img>
+      <h1>HOME</h1>
+      <p> Phase1: Fetching data from Steam Web API</p>
+      <div></div>
     </>
   );
 }
