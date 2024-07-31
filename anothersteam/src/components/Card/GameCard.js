068e02018a6efddbcadd78c6334528128338ea25
n106ky (N.C) <n106ky@gmail.com>
2024-07-31 16:50:23 -0400

    Update: most-played-games

--- anothersteam/src/components/Card/GameCard.js
+++ anothersteam/src/components/Card/GameCard.js
@@ -4,10 +4,10 @@ export default function GameCard({ games = [] }) {
   console.log("(GameCard) games", games);
 
   return (
-    <div className="mostPlayedGames-container">
+    <div>
       {Array.isArray(games) && games.length > 0 ? (
         games.map((game) => (
-          <div key={game.id} className="card">
+          <div key={game.id} className="gamecard">
             <p>PHOTO: 'img' </p>
             <p>ID: {game.id}</p>
             <p>NAME: {game.name}</p>
