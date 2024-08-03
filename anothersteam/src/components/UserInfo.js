3e5b1d15b0f9411349060fc51e178677ad35dd27
ev <etersh810@gmail.com>
2024-08-03 18:12:11 -0400

    Modify UserInfo to show the profile image first

--- anothersteam/src/components/UserInfo.js
+++ anothersteam/src/components/UserInfo.js
@@ -1,55 +1,59 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 const UserInfo = () => {
-	const [user, setUser] = useState([]);
-	const [error, setError] = useState(null);
+  const [user, setUser] = useState([]);
+  const [error, setError] = useState(null);
 
-	useEffect(() => {
-		fetch("/api/user-info")
-			.then((res) => res.json())
-			.then((data) => {
-				if (data.err) {
-					throw new Error(data.err);
-				}
-				setUser(data.userData);
-			})
-			.catch((err) => setError(err.message));
-	}, []);
+  useEffect(() => {
+    fetch('/api/user-info')
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.err) {
+          throw new Error(data.err);
+        }
+        setUser(data.userData);
+      })
+      .catch((err) => setError(err.message));
+  }, []);
 
-	if (error) return <div>Error: {error}</div>;
+  if (error) return <div>Error: {error}</div>;
 
-	const getFlag = (countryCode) => {
-		return countryCode !== "Not Available"
-			? `https://flagsapi.com/${countryCode}/flat/32.png`
-			: null;
-	};
-	//theme:flat/shiny supported
-	//size:16/24/32/48/64px supported
+  const getFlag = (countryCode) => {
+    return countryCode !== 'Not Available'
+      ? `https://flagsapi.com/${countryCode}/flat/32.png`
+      : null;
+  };
+  //theme:flat/shiny supported
+  //size:16/24/32/48/64px supported
 
-	return (
-		<div>
-			<div className="user-info">
-				<h2>
-					{user.name}
-					<img src={user.profilePic} style={{ marginLeft: "8px" }} />
-				</h2>
-				<p>Name: {user.realName}</p>
-				<p>Steam ID: {user.steamID}</p>
-				<p>
-					Country:{" "}
-					{user.countryCode !== "Not Available" ? (
-						<img
-							src={getFlag(user.countryCode)}
-							style={{ marginLeft: "8px" }}
-						/>
-					) : (
-						"Not Available"
-					)}
-				</p>
-				<p>City: {user.cityID}</p>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="user-info">
+        <h2>
+          {' '}
+          <img
+            src={user.profilePic}
+            style={{ marginLeft: '8px', marginRight: '10px', width: '50px' }}
+          />
+          {user.name}
+        </h2>
+        <p>Name: {user.realName}</p>
+        <p>Steam ID: {user.steamID}</p>
+        <p>
+          Country:{' '}
+          {user.countryCode !== 'Not Available' ? (
+            <img
+              src={getFlag(user.countryCode)}
+              style={{ marginLeft: '8px' }}
+            />
+          ) : (
+            'Not Available'
+          )}
+        </p>
+        <p>City: {user.cityID}</p>
+      </div>
+    </div>
+  );
 };
 
 export default UserInfo;
