a0b0b452102114d6c7c3c09d291f06882ba44c7f
n106ky (N.C) <n106ky@gmail.com>
2024-08-09 12:35:29 -0400

    Minor updates

--- anothersteam/src/pages/user/login.js
+++ anothersteam/src/pages/user/login.js
@@ -49,29 +49,31 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register("username", { required: "Username is required" })}
-        placeholder="Username"
-        type="text"
-      />
-      {errors.username && <p>{errors.username.message}</p>}
+    <div className="login-container">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("username", { required: "Username is required" })}
+          placeholder="Username"
+          type="text"
+        />
+        {errors.username && <p>{errors.username.message}</p>}
 
-      <input
-        {...register("password", {
-          required: "Password is required",
-          minLength: {
-            value: 8,
-            message: "Password must be at least 8 characters long",
-          },
-        })}
-        placeholder="Password"
-        type="password"
-      />
-      {errors.password && <p>{errors.password.message}</p>}
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters long",
+            },
+          })}
+          placeholder="Password"
+          type="password"
+        />
+        {errors.password && <p>{errors.password.message}</p>}
 
-      <button type="submit">Login</button>
-    </form>
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 };
 
