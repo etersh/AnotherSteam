63062267aac2fcd96c8a9a41160446c5bbe39558
n106ky (N.C) <n106ky@gmail.com>
2024-08-14 09:14:35 -0400

    Update comments

--- anothersteam/src/state/store.js
+++ anothersteam/src/state/store.js
@@ -12,17 +12,17 @@ export const favoriteAtom = atom([]);
 // USER
 
 // Initialize from localStorage if available
-export const userTokenAtom = atom(
+export const userTokenAtom = atom( // storing user token (undecoded)
   typeof window !== "undefined" ? localStorage.getItem("userJWT") : null
 );
 // authentication base on if userJWT is found
 export const isAuthenticatedAtom = atom((get) => !!get(userTokenAtom));
 
-export const userAtom = atom(
+export const userAtom = atom( // storing user steam id after retrieving it from Database
   typeof window !== "undefined" ? localStorage.getItem("userSteamid") : null
 );
 
-export const steamUserAtom = atom(
+export const steamUserAtom = atom( // storing whole user steam data after API call
   typeof window !== "undefined"
     ? JSON.parse(localStorage.getItem("userSteamData"))
     : null
